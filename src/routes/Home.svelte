25f27ee58aadd93d24a9c6059f65cdaf3898fbce
Tieysson Cardoso Silva <50223152+Tieysson@users.noreply.github.com>
2022-05-16 12:06:55 +0000

    Add files via upload

---
+++ src/routes/Home.svelte
@@ -0,0 +1,10 @@
+<script>
+	import { HeartIcon } from "svelte-feather-icons";
+</script>
+
+<div class="h1">
+	Hello World! <HeartIcon />
+</div>
+
+<style>
+</style>
---
+++ src/routes/NotFound.svelte
@@ -0,0 +1,9 @@
+<script>
+</script>
+
+<div>
+	<h2>404 - Not Found</h2>
+</div>
+
+<style>
+</style>
