e285370caa1c3f85503813b0bb51c597bd15a62b
Tieysson Cardoso Silva <50223152+Tieysson@users.noreply.github.com>
2022-05-16 12:06:23 +0000

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,17 @@
 <script>
+	import Router from "svelte-spa-router";
+	import Home from "./routes/Home.svelte";
+	import NotFound from "./routes/NotFound.svelte";
+
+	let routes = {
+		"/": Home,
+
+		"*": NotFound,
+	};
 </script>
 
 <main>
+	<Router {routes} />
 </main>
 
 <style>
